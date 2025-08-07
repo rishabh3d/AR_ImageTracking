80bd7d180c48dd88f305887a84000f765be139a1
Prashant <prashant1998gupta@gmail.com>
2025-08-07 22:47:35 +0530

    added

--- Assets/Imagine/Common/Scripts/Helpers/DemoSceneLoader.cs
+++ Assets/Imagine/Common/Scripts/Helpers/DemoSceneLoader.cs
@@ -4,16 +4,16 @@ using UnityEngine.SceneManagement;
 
 public class DemoSceneLoader : MonoBehaviour
 {
-#if UNITY_WEBGL && !UNITY_EDITOR
-    [DllImport("__Internal")]
-    private static extern void CleanupWebGLMemory();
-#endif
+//#if UNITY_WEBGL && !UNITY_EDITOR
+//    [DllImport("__Internal")]
+//    private static extern void CleanupWebGLMemory();
+//#endif
 
     public void LoadScene(string sceneName)
     {
-#if UNITY_WEBGL && !UNITY_EDITOR
-        CleanupWebGLMemory(); // JS-side cleanup
-#endif
+//#if UNITY_WEBGL && !UNITY_EDITOR
+//        CleanupWebGLMemory(); // JS-side cleanup
+//#endif
         SceneManager.LoadScene(sceneName);
     }
 }
