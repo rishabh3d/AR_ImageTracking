25eae78b21229d4a28fc365376018b53dfae4eef
Prashant Gupta <57870705+prashant1998gupta@users.noreply.github.com>
2025-08-07 15:39:26 +0530

    added

--- Assets/Imagine/Common/Scripts/Helpers/DemoSceneLoader.cs
+++ Assets/Imagine/Common/Scripts/Helpers/DemoSceneLoader.cs
@@ -8,18 +8,9 @@ namespace Imagine.WebAR.Samples
 {
     public class DemoSceneLoader : MonoBehaviour
     {
-        public GameObject firstPanel;
         public void LoadScene(string sceneName)
         {
             SceneManager.LoadScene(sceneName);
-        }
-
-        public void PanelLoad(string panelName)
-        {
-           if(firstPanel != null)
-           {
-               firstPanel.SetActive(panelName == "FirstPanel");
-           }
         }
     }
 }
