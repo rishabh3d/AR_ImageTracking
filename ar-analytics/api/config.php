07bcffff3cb04cac687a1d9b78b71cea6ab5d9ba
Prashant Gupta <57870705+prashant1998gupta@users.noreply.github.com>
2026-04-03 15:33:40 +0530

    Prashant - add new porject for analitics

--- ar-analytics/api/config.php
+++ ar-analytics/api/config.php
@@ -5,15 +5,15 @@
  */
 
 // ─── Database Settings ───
-define('DB_HOST', 'sql309.infinityfree.com');          // Hostinger usually uses localhost
-define('DB_NAME', 'if0_41513788_my_ar_data');      // Your database name
-define('DB_USER', 'if0_41513788');                  // From Hostinger hPanel → Databases
-define('DB_PASS', 'wyjlNlM5L4m');                  // From Hostinger hPanel → Databases
+define('DB_HOST', 'localhost');          // Hostinger usually uses localhost
+define('DB_NAME', 'u668316061_aranalytics');      // Your database name from Hostinger
+define('DB_USER', 'u668316061_prashant1998');     // From Hostinger hPanel → Databases
+define('DB_PASS', 'Pawrs@1995');                  // Final production password
 define('DB_CHARSET', 'utf8mb4');
 
 // ─── App Settings ───
 define('APP_NAME', 'AR Analytics');
-define('APP_URL', 'http://myaranalytics.gamer.gd'); // Your analytics URL
+define('APP_URL', 'https://dashboard.rionick.com'); // Your analytics URL
 define('APP_SECRET', 'ar_secret_64_prashant_2024_tracker_key'); // Used for token signing
 
 // ─── Session Settings ───
